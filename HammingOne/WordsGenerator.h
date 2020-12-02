561bbd352d735e4f452b700d7488f633cca49381
Pawel Pohl <pawel.pohl229@gmail.com>
2020-12-02 13:30:58 +0100

    Code clean-up and comments added

--- HammingOne/WordsGenerator.h
+++ HammingOne/WordsGenerator.h
@@ -9,6 +9,8 @@
 #include <fstream>
 #include "Stopwatch.h"
 
+// This class can generate M unique binary words of length N as a vector of bitsets, or vector of int subwords in the form of AoS and SoA
+// It can also generate pairs of words with Hamming distance of 1 with an O(M^2 * N)  sequential algorithm and save the pairs to a file
 template <int N, int M>
 class WordsGenerator
 {
@@ -19,63 +21,31 @@ public:
 	std::vector<std::bitset<N>> words;
 
 	WordsGenerator(std::string words_file_name, std::string pairs_file_name) : words_file_name{ words_file_name }, pairs_file_name{ pairs_file_name } {};
-	//std::unordered_set<std::bitset<N>> generateWords();
+	// Generate subwords as an AoS
 	std::vector<unsigned int> generateWordsForGPU();
+	// Generate subwords as an SoA
 	std::vector<std::vector<unsigned int>> generateWordsForGPUStrieded();
-	void WordsGenerator<N, M>::generatePairsForGPU(unsigned int* output, int output_size, int stride);
+	// Geberate pairs and save them to a file
 	void generatePairs();
+
 private:
+	// Given a word create a copy of the word with bit changeBitIndex set to 1
 	std::bitset<N> createAlteredWord(std::bitset<N> const& source, int changeBitIndex);
+	// Caluclate the Hamming distance between two words
 	int hammingDistance(std::bitset<N> const& lhs, std::bitset<N> const& rhs);
+	// Add the word to the queue and to the AoS vector with subwords
 	void saveWord(std::queue<std::bitset<N>>&, std::vector<unsigned int>&, const std::bitset<N>&);
+	// Add the word to the queue, to the AoS vector with subwords and to the internal vector of bitsets
 	void saveWord(std::queue<std::bitset<N>>&, std::vector<unsigned int>&, std::vector<std::bitset<N>>&, const std::bitset<N>&);
+	// Add the word to the queue, to the SoA vector with subwords and to the internal vector of bitsets
 	void saveWordStride(std::queue<std::bitset<N>>&, std::vector<std::bitset<N>>&, std::vector<std::vector<unsigned int>>&, const std::bitset<N>&);
 
 };
 
-//template <int N, int M>
-//std::unordered_set<std::bitset<N>> WordsGenerator<N, M>::generateWords()
-//{
-//	Stopwatch stopwatch;
-//	long long iter = 0;
-//	auto result = words.emplace(std::bitset<N>());
-//	std::ofstream wordFile;
-//	wordFile.open(words_file_name);
-//	std::queue<std::bitset<N>> generatingWords;
-//	generatingWords.push(*result.first);
-//	wordFile << *result.first << std::endl;
-//
-//	std::cout << "Genrating..." << std::endl;
-//	stopwatch.Start();
-//	while (this->words.size() < M)
-//	{
-//		std::bitset<N>& generatingWord = generatingWords.front();
-//
-//		for (size_t i = 0; i < N; i++)
-//		{
-//			if (!generatingWord[i])
-//			{
-//				iter++;
-//				auto result = words.emplace(createAlteredWord(generatingWord, i));
-//				if (result.second)
-//				{
-//					generatingWords.push(*result.first);
-//					wordFile << *result.first << std::endl;
-//				}
-//			}
-//		}
-//		generatingWords.pop();
-//	}
-//	stopwatch.Stop();
-//	wordFile.close();
-//
-//	std::cout << "Total iter: " << iter << " words:" << this->words.size() << std::endl;
-//	return words;
-//}
-
 template <int N, int M>
 std::vector<unsigned int> WordsGenerator<N, M>::generateWordsForGPU()
 {
+
 	std::queue<std::bitset<N>> generatingWords;
 	std::vector<unsigned int> wordsGPU;
 	std::unordered_set<std::bitset<N>> wordsSet;
@@ -88,9 +58,14 @@ std::vector<unsigned int> WordsGenerator<N, M>::generateWordsForGPU()
 	std::cout << "Genrating..." << std::endl;
 	stopwatch.Start();
 
+	// Create a 0 word and add it 
 	auto result = wordsSet.emplace(std::bitset<N>());
 	saveWord(generatingWords, wordsGPU, words, *result.first);
 
+	// Generate next words until the size is obtained
+	// at each iteration take the first element from the queue which stores next generating words
+	// and create at possible words from it i.e. switch bits set to 0 and add the new word to the vectors and
+	// to the queue as the possible next generating word
 	while (wordsSet.size() < M)
 	{
 		std::bitset<N>& generatingWord = generatingWords.front();
@@ -126,7 +101,7 @@ std::vector<std::vector<unsigned int>> WordsGenerator<N, M>::generateWordsForGPU
 	std::vector<std::vector<unsigned int>> wordsGPUStride;
 	std::unordered_set<std::bitset<N>> wordsSet;
 
-
+	// First alocate vectors for subwords - i'th vector stores i'th subwords of each word
 	for (size_t i = 0; i < ceil(N / 32.0); i++)
 		wordsGPUStride.push_back(std::vector<unsigned int>());
 
@@ -138,9 +113,14 @@ std::vector<std::vector<unsigned int>> WordsGenerator<N, M>::generateWordsForGPU
 	std::cout << "Genrating..." << std::endl;
 	stopwatch.Start();
 
+	// Create a 0 word and add it
 	auto result = wordsSet.emplace(std::bitset<N>());
 	saveWordStride(generatingWords, words, wordsGPUStride, *result.first);
 
+	// Generate next words until the size is obtained
+	// at each iteration take the first element from the queue which stores next generating words
+	// and create at possible words from it i.e. switch bits set to 0, add the new word to the vectors and
+	// to the queue as the possible next generating word
 	while (wordsSet.size() < M)
 	{
 		std::bitset<N>& generatingWord = generatingWords.front();
@@ -175,6 +155,7 @@ void WordsGenerator<N, M>::saveWord(std::queue<std::bitset<N>>& generatingWords,
 {
 	generatingWords.push(word);
 	unsigned int subword = 0;
+	// Split the word into 32 bit subwords and save each bit from the bitset as a bit in a UINT 
 	for (size_t i = 0; i < N; i++)
 	{
 		if (i > 0 && i % 32 == 0)
@@ -186,7 +167,6 @@ void WordsGenerator<N, M>::saveWord(std::queue<std::bitset<N>>& generatingWords,
 	}
 	wordsGPU.emplace_back(subword);
 }
-
 
 template <int N, int M>
 void WordsGenerator<N, M>::saveWord(std::queue<std::bitset<N>>& generatingWords, std::vector<unsigned int>& wordsGPU, std::vector<std::bitset<N>>& words, const std::bitset<N>& word)
@@ -201,6 +181,8 @@ void WordsGenerator<N, M>::saveWordStride(std::queue<std::bitset<N>>& generating
 	generatingWords.push(word);
 	words.push_back(word);
 	unsigned int subword = 0;
+	// Split the word into 32 bit subwords and save each bit from the bitset as a bit in a UINT 
+	// each subword is stored in the corresponding vector of subwords
 	for (size_t i = 0; i < N; i++)
 	{
 		if (i > 0 && i % 32 == 0)
@@ -213,7 +195,6 @@ void WordsGenerator<N, M>::saveWordStride(std::queue<std::bitset<N>>& generating
 	wordsGPU[wordsGPU.size() - 1].emplace_back(subword);
 }
 
-
 template <int N, int M>
 std::bitset<N> WordsGenerator<N, M>::createAlteredWord(std::bitset<N> const& source, int changeBitIndex)
 {
@@ -221,7 +202,6 @@ std::bitset<N> WordsGenerator<N, M>::createAlteredWord(std::bitset<N> const& sou
 	result[changeBitIndex] = 1;
 	return result;
 }
-
 
 template <int N, int M>
 void WordsGenerator<N, M>::generatePairs()
@@ -235,6 +215,8 @@ void WordsGenerator<N, M>::generatePairs()
 	std::cout << "Generating pairs" << std::endl;
 	stopwatch.Start();
 
+	// For each word iterate through all words with index bigger than the checked word
+	// for each pair of words check their distance and write the pair to the file if the distance between them is 1
 	for (auto const& elemDim1 : this->words)
 	{
 		for (auto it = innerIterator; it != words.end(); it++)
@@ -254,44 +236,6 @@ void WordsGenerator<N, M>::generatePairs()
 	wordPairsFile.close();
 }
 
-template <int N, int M>
-void WordsGenerator<N, M>::generatePairsForGPU(unsigned int* output, int output_size, int stride)
-{
-	Stopwatch stopwatch;
-	std::ofstream wordPairsFile;
-	wordPairsFile.open(pairs_file_name);
-	auto innerIterator = words.begin()++;
-
-	int wordIdx = 0, count = 0;
-
-	std::cout << "Generating pairs" << std::endl;
-	stopwatch.Start();
-
-	for (auto const& elemDim1 : this->words)
-	{
-		for (auto it = innerIterator; it != words.end(); it++)
-		{
-
-			int dist = hammingDistance(elemDim1, *it);
-			if (dist == 1)
-			{
-				//int index = it - words.begin();
-				count++;
-				wordPairsFile << elemDim1 << ';' << *it << std::endl;
-				//output[wordIdx * stride + index / 32] += 1 << 31 - index % 32;
-			}
-
-		}
-		innerIterator++;
-		wordIdx++;
-		//wordPairsFile << std::endl;
-	}
-	//wordPairsFile.write((char*)output, output_size);
-	stopwatch.Stop();
-	std::cout << count << " pairs found" << std::endl;
-
-	wordPairsFile.close();
-}
 
 template <int N, int M>
 int WordsGenerator<N, M>::hammingDistance(std::bitset<N> const& lhs, std::bitset<N> const& rhs)
--- HammingOne/kernel.cu
+++ HammingOne/kernel.cu
@@ -14,30 +14,38 @@
 #include <thrust/host_vector.h>
 #include <thrust/device_vector.h>
 
+// constants specifying words characteristics
+
 const int word_size = 1000;
 const int words_count = 100000;
 const int subword_size = 32;
 const int subwords_count = (int)ceil(word_size / (double)subword_size);
 
-const std::string words_file_name = std::string("./") + std::to_string(word_size) + std::string("-") + std::to_string(words_count) + std::string("-words.txt");
-const std::string pairs_file_name = std::string("./") + std::to_string(word_size) + std::string("-") + std::to_string(words_count) + std::string("-pairs.txt");
-const std::string pairs_file_name_kernel = std::string("./") + std::to_string(word_size) + std::string("-") + std::to_string(words_count) + std::string("-kpairs.txt");
-const std::string pairs_file_name_kernel_verbose = std::string("./") + std::to_string(word_size) + std::string("-") + std::to_string(words_count) + std::string("-kvpairs.csv");
+
+// Names of files used for saving the words and pairs of words for the generator and the kernel
+const std::string words_file_name = std::string("./data/") + std::to_string(word_size) + std::string("-") + std::to_string(words_count) + std::string("-words.txt");
+const std::string pairs_file_name = std::string("./data/") + std::to_string(word_size) + std::string("-") + std::to_string(words_count) + std::string("-pairs.txt");
+const std::string pairs_file_name_kernel = std::string("./data/") + std::to_string(word_size) + std::string("-") + std::to_string(words_count) + std::string("-kpairs.txt");
+const std::string pairs_file_name_kernel_verbose = std::string("./data/") + std::to_string(word_size) + std::string("-") + std::to_string(words_count) + std::string("-kvpairs.csv");
 
 WordsGenerator<word_size, words_count> generator("", "");
+
+// Forward declarations of funcations
 
 std::vector<unsigned> loadWordsForGPU();
 std::vector<std::vector<unsigned int>> loadWordsForGPUStride();
 thrust::device_vector<unsigned int> copyWordsToGPU(std::vector<unsigned int> generatedWords);
 thrust::device_vector<unsigned int> copyWordsToGPUStride(std::vector<std::vector<unsigned int>> generatedWords);
-void loadPairs(unsigned int* output, int output_size, int stride);
-//void readPairs();
-//std::unordered_set<std::bitset<word_size>> loadWords();
-//thrust::device_vector<unsigned int> parseWord(std::string input);
-//void parseWord(std::string input, thrust::device_vector<unsigned int>& words);
-//thrust::device_vector<unsigned int> copyWordsToGPU();
-//thrust::device_vector<unsigned int> copyWordsToGPU(std::unordered_set<std::bitset<word_size>> generatedWords);
+void generatePairsCPU();
 
+
+/* GPU kernel finding words which are in distance 1
+*  words is an array with 32-bit subwords, where each consequtive subword of a word is strided by wordsCount * 4 bits
+*  output is an n x ceil(n / 32) matrix, where each row represents a word and each bit of the row is indicating whether word i and j are in distance 1 
+*  Each thread represents one word for which it then checks the distance with all words which have higher index that the particular word
+*  If the words are in distance 1 the thread sets a corresponding bit in the output array to 1 
+*  The distance is calucated as the number of bits set to 1 in the XOR operation of two subwords
+*/
 __global__ void searchHammingOne(unsigned int* words, unsigned int* output, unsigned int wordsCount, unsigned int subwords_count, unsigned int ints_per_words_count, unsigned int bits_per_subword)
 {
 	int wordIndex = threadIdx.x + blockIdx.x * blockDim.x;
@@ -47,47 +55,34 @@ __global__ void searchHammingOne(unsigned int* words, unsigned int* output, unsi
 
 	unsigned int* word = new unsigned int[subwords_count];
 
-	//for (size_t i = 0; i < subwords_count; i++)
-	//{
-	//	word[i] = words[wordIndex * subwords_count + i];
-	//}
-
 	int checkedIndex = wordIndex + 1;
 	unsigned int distance;
 	int offset, value, index = wordIndex * ints_per_words_count;
 
-	//while (checkedIndex < wordsCount)
-	//{
-	//	distance = 0;
-	//	for (size_t i = 0; i < subwords_count && distance < 2; i++)
-	//	{
-	//		distance += __popc(word[i] ^ words[subwords_count * checkedIndex + i]);
-	//	}
-	//	if (!(distance >> 1))
-	//	{
-	//		offset = checkedIndex / bits_per_subword;
-	//		value = 1 << bits_per_subword - 1 - checkedIndex % bits_per_subword;
-	//		output[index + offset] |= value;
-	//	}
-	//	checkedIndex++;
-	//}
-
+	// First save the word assigned to the thread in local mememory
+	// It's going to be used at each itteration of the loop
 	for (size_t i = 0; i < subwords_count; i++)
-	{
 		word[i] = words[wordIndex + i * wordsCount];
-	}
 
+	// Then go through all words which have index bigger than the assigned word
 	while (checkedIndex < wordsCount)
 	{
 		distance = 0;
+		// Go through all subwords of the checkedIndex word
 		for (size_t i = 0; i < subwords_count && distance < 2; i++)
 		{
+			// add the number of bits set to 1 in the XOR of two subwords
 			distance += __popc(word[i] ^ words[checkedIndex + i * words_count]);
 		}
-		if (!(distance >> 1))
+
+		if (distance == 1)
 		{
-			offset = checkedIndex / bits_per_subword;
-			value = 1 << bits_per_subword - 1 - checkedIndex % bits_per_subword;
+			// the checkedIndex bit from start of the array for the assigned word has to be set
+			// it's localted in the checkedIndex / 32 int in the array (the result is automaticaly floored)
+			offset = checkedIndex / 32;
+			// value represent an UINT with the correspoding bit set to 1
+			value = 1 << 31 - checkedIndex % 32;
+			// only one bit is set, so the result can be ORed, which is faster than adding
 			output[index + offset] |= value;
 		}
 		checkedIndex++;
@@ -97,24 +92,32 @@ __global__ void searchHammingOne(unsigned int* words, unsigned int* output, unsi
 	delete[] word;
 }
 
-#define VERBOSE_PAIRS
+
+// Uncomment to save pairs generatored by the kernel to a file
+//#define VERBOSE_PAIRS
 int main()
 {
 	generator = WordsGenerator<word_size, words_count>(words_file_name, pairs_file_name);
-	std::cout << "Reading data..." << std::endl;
-	//auto generatedWords = loadWordsForGPU();
-	//auto words = copyWordsToGPU(generatedWords);
+
+	// Get words from the generator
+	std::cout << "Reading data...\n\n";
 	auto generatedWords = loadWordsForGPUStride();
 	auto words = copyWordsToGPUStride(generatedWords);
 	auto wordsPtr = thrust::raw_pointer_cast(words.begin().base());
+	//loadPairs();
 	std::cout << "Done!" << std::endl;
 
 
-	// adjust the demensions to size of ints - each bits represents one word
+	//Calculate the size of the output matrix for kernel
+
+	// ints needed to store one row of output - adjusted to size of ints
 	const size_t ints_per_words_count = ceil(words_count / 32.0);
+	// total number of ints needed to represent the output
 	const size_t output_ints_count = words_count * ints_per_words_count;
+	// total number of bytes needed to represent the output
 	const size_t output_size = output_ints_count * sizeof(int);
-	int threads = 512;
+
+	int threads = 256;
 	int blocks = (int)ceil(words.size() / (double)threads);
 
 	std::cout << "Words count: " << words_count << std::endl;
@@ -124,26 +127,23 @@ int main()
 	cudaMalloc(&d_output, output_size);
 	h_output = new unsigned int[output_ints_count]();
 
-	//loadPairs(h_output, output_size, ints_per_words_count);
 
 	float time;
 	cudaEvent_t start, stop;
 	cudaEventCreate(&start);
 	cudaEventCreate(&stop);
 
-	//std::ofstream wordPairsFile;
-	//wordPairsFile.open(pairs_file_name_kernel);
+	// This stream is used for saving pairs generated by the kernel
 	std::ofstream wordPairsFileVerbose;
 	wordPairsFileVerbose.open(pairs_file_name_kernel_verbose);
 
-	//for (size_t i = 0; i < 3; i++)
-	//{
+
 	cudaFuncSetCacheConfig(searchHammingOne, cudaFuncCachePreferL1);
 	cudaMemset(d_output, 0, output_size);
 
-
+	std::cout << "\nCuda start" << std::endl;
 	cudaEventRecord(start, 0);
-	searchHammingOne << <blocks, threads >> > (wordsPtr, d_output, words_count, subwords_count, ints_per_words_count, subword_size);
+	searchHammingOne <<< blocks, threads >>> (wordsPtr, d_output, words_count, subwords_count, ints_per_words_count, subword_size);
 	cudaEventRecord(stop, 0);
 
 	cudaError_t err = cudaGetLastError();
@@ -156,60 +156,63 @@ int main()
 	cudaDeviceSynchronize();
 	cudaEventElapsedTime(&time, start, stop);
 
-	std::cout << "B: " << std::setw(3) << blocks << " T: " << std::setw(4) << threads << " time: " << time << std::endl;
-
-	//wordPairsFile.write((char*)h_output, output_size);
+	std::cout << "B: " << std::setw(3) << blocks << " T: " << std::setw(4) << threads << " time: " << time << " ms" << std::endl;
 
 	int parallel_count = 0;
 
+	// This loop counts all 1's set in the output generated in the kernel
+	// which correspond to the number of found pairs
+	// The loop can also print all pairs to a file
 	for (size_t i = 0; i < words_count; i++)
 	{
 		for (size_t j = 0; j < ints_per_words_count; j++)
 		{
 			parallel_count += __popcnt(h_output[i * ints_per_words_count + j]);
-#ifdef VERBOSE_PAIRS
+			#ifdef VERBOSE_PAIRS
 			for (int k = 0; k < subword_size; k++)
 			{
 				if (h_output[i * ints_per_words_count + j] & 1 << 31 - k)
 					wordPairsFileVerbose << generator.words[i] << ';' << generator.words[j * subword_size + k] << std::endl;
 			}
-#endif
+			#endif
 		}
 	}
 
 	std::cout << "B: " << std::setw(3) << blocks << " T: " << std::setw(4) << threads << " count parallel: " << std::setw(6) << parallel_count << std::endl;
-	//}
+
 	cudaEventDestroy(start);
 	cudaEventDestroy(stop);
 	cudaFree(d_output);
-	//wordPairsFile.close();
 
 	delete[] h_output;
 	return 0;
 }
 
-
+// Load words from generator as AoS
 std::vector<unsigned> loadWordsForGPU()
 {
 	return generator.generateWordsForGPU();
 }
 
+// Load words from generator as SoA
 std::vector<std::vector<unsigned int>> loadWordsForGPUStride()
 {
 	return generator.generateWordsForGPUStrieded();
 }
 
-
-void loadPairs(unsigned int* output, int output_size, int stride)
+// Generate pairs using the CPU
+// Words must be generated first
+void generatePairsCPU()
 {
-	return generator.generatePairsForGPU(output, output_size, stride);
+	generator.generatePairs();
 }
 
+// Copy AoS data created by the generator to a thrust device vector
 thrust::device_vector<unsigned int> copyWordsToGPU(std::vector<unsigned int> generatedWords)
 {
 	Stopwatch stopwatch;
 	thrust::device_vector<unsigned int> words;
-	std::cout << "Copying to GPU memory" << std::endl;
+	std::cout << "\nCopying to GPU memory" << std::endl;
 	stopwatch.Start();
 
 	words.insert(words.begin(), generatedWords.begin(), generatedWords.end());
@@ -218,133 +221,22 @@ thrust::device_vector<unsigned int> copyWordsToGPU(std::vector<unsigned int> gen
 	return words;
 }
 
+// Copy SoA data created by the generator to a thrust device vector
 thrust::device_vector<unsigned int> copyWordsToGPUStride(std::vector<std::vector<unsigned int>> generatedWords)
 {
 	Stopwatch stopwatch;
 	thrust::device_vector<unsigned int> words;
-	std::cout << "Copying to GPU memory" << std::endl;
+	std::cout << "\nCopying to GPU memory" << std::endl;
 	stopwatch.Start();
 
 	for (auto & subwords : generatedWords)
-	{
-		//if (words.size() == 0)
-		//	words.insert(words.begin(), subwords.begin(), subwords.end());
-		//else 
-			words.insert(words.end(), subwords.begin(), subwords.end());
-
-	}
+		words.insert(words.end(), subwords.begin(), subwords.end());
 
 	stopwatch.Stop();
+	std::cout << std::endl;
 
 	return words;
 }
-
-
-//void readPairs()
-//{
-//	std::ifstream pairsStream{ pairs_file_name };
-//	//Stopwatch stopwatch;
-//	std::string word;
-//
-//	//stopwatch.Start();
-//	while (!pairsStream.eof())
-//	{
-//		std::getline(pairsStream, word, ';');
-//		//parseWord(word);
-//		//std::cout << "word" << word << std::endl;
-//		//std::cout << std::endl;
-//		std::getline(pairsStream, word);
-//		//parseWord(word);
-//		//std::cout << word << std::endl;
-//	}
-//
-//	//stopwatch.Stop();
-//	pairsStream.close();
-//}
-
-//thrust::device_vector<unsigned int> parseWord(std::string input)
-//{
-//	// '0' = 48/ 0b0011000
-//	// '1' = 49/ 0b0011001 
-//	// Works faste than stoi with base 2!
-//	const unsigned char zeroAsciiCode = 48;
-//	thrust::device_vector<unsigned int> word;
-//
-//	unsigned int maxValue = pow(2, subword_size - 1);
-//	unsigned int currentValue = maxValue;
-//	word.push_back(0);
-//	auto index = 0;
-//	for (auto letter : input)
-//	{
-//		if (!currentValue)
-//		{
-//			currentValue = maxValue;
-//			word.push_back(0);
-//			//std::cout << word[index] << std::endl;
-//			index++;
-//		}
-//		if (letter - zeroAsciiCode)
-//			word[index] += currentValue;
-//		currentValue >>= 1;
-//	}
-//
-//	return word;
-//}
-
-//std::unordered_set<std::bitset<word_size>> loadWords()
-//{
-//	auto generator = WordsGenerator<word_size, words_count>(words_file_name, pairs_file_name);
-//	return generator.generateWords();
-//	//generator.generatePairs();
-//}
-
-//thrust::device_vector<unsigned int> copyWordsToGPU(std::unordered_set<std::bitset<word_size>> generatedWords)
-//{
-//
-//	Stopwatch stopwatch;
-//
-//	thrust::device_vector<unsigned int> words;
-//
-//	stopwatch.Start();
-//	unsigned int subword;
-//	for (auto word : generatedWords)
-//	{
-//		subword = 0;
-//		for (size_t i = 0; i < word_size; i++)
-//		{
-//			if (i > 0 && i % subword_size == 0)
-//			{
-//				words.push_back(subword);
-//				subword = 0;
-//			}
-//			subword |= word[i] << subword_size - 1 - i % subword_size;
-//		}
-//		words.push_back(subword);
-//	}
-//	stopwatch.Stop();
-//
-//	return words;
-//}
-
-//thrust::device_vector<unsigned int> copyWordsToGPU()
-//{
-//	std::ifstream wordsStream{ words_file_name };
-//	Stopwatch stopwatch;
-//	std::string word;
-//	thrust::device_vector<unsigned int> words;
-//
-//	stopwatch.Start();
-//	while (!wordsStream.eof())
-//	{
-//		std::getline(wordsStream, word);
-//		if (!word.empty()) parseWord(word, words);
-//		//std::cout << "word: " << word << std::endl;
-//	}
-//
-//	stopwatch.Stop();
-//	wordsStream.close();
-//	return words;
-//}
 
 //void parseWord(std::string input, thrust::device_vector<unsigned int>& words)
 //{
